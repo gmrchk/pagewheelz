0208548b5ae47259a4a2f8dd7cefc658ec5470ac
Georgy Marchuk <gmarcuk@gmail.com>
2018-12-02 15:04:01 +0100

    update wheelz

--- dist/pagewheelz.js
+++ dist/pagewheelz.js
@@ -383,7 +383,7 @@ var Wheelz = function () {
             this._resizeHandler();
 
             // wheel
-            this.element.addEventListener('mousewheel', this._wheelHandler);
+            this.element.addEventListener('wheel', this._wheelHandler);
 
             if (this.options.draggable) {
                 // drag
@@ -444,9 +444,7 @@ var Wheelz = function () {
             this.element.style.transform = "";
 
             // wheel
-            this.element.removeEventListener('mousewheel', this._wheelHandler);
-
-            this.element.removeEventListener('mousewheel', this._wheelHandler);
+            this.element.removeEventListener('wheel', this._wheelHandler);
 
             if (this.options.draggable) {
                 // drag
